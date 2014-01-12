7b51d6d0c3e614bd0db213788268db9b3c98bea3
Ricard Wanderlof <ricard2013@butoba.net>
2014-01-12 23:29:08 +0100

    Add actual adjustment list
    * For each object, have an id and adjustment struct. Probably not the best
      implementation, but good enough for now.
    * Rudimentary display of adjustment list to verify it is created properly.

--- midicontroller.c
+++ midicontroller.c
@@ -1,36 +1,66 @@
 #include <gtk/gtk.h>
 
+struct adjustor {
+  const char *id;
+  GtkAdjustment *adj;
+};
+
 void 
 on_winMain_destroy (GtkObject *object, gpointer user_data)
 {
   gtk_main_quit();
 }
 
-void add_adjustments(GList *widget_list);
+void add_adjustments(GList *widget_list, GList **adj_list);
 
 void create_adjustment (gpointer data, gpointer user_data)
 {
   GtkWidget *this = data;
+  GList **adj_list = user_data;
 
   const char *id = gtk_buildable_get_name(GTK_BUILDABLE(this));
 
   printf("Widget: %s, id %s\n", gtk_widget_get_name(this), id ? id : "none");
 
+  if (id && strncmp(id, "mcw", 3) == 0) {
+    GObject *adjobj;
+    struct adjustor *adjustor = g_new(struct adjustor, 1);
+    g_object_get(this, "adjustment", &adjobj, NULL);
+    if (adjobj && GTK_IS_ADJUSTMENT(adjobj)) {
+      printf("has adjustment\n");
+      adjustor->id = id;
+      adjustor->adj = GTK_ADJUSTMENT (adjobj);
+      *adj_list = g_list_append(*adj_list, adjustor);
+    }
+  }
+
   if (GTK_IS_CONTAINER(this)) {
      printf("It's a container\n");
-     add_adjustments(gtk_container_get_children(GTK_CONTAINER(this)));
+     add_adjustments(gtk_container_get_children(GTK_CONTAINER(this)), adj_list);
   }
 }
 
-void add_adjustments (GList *widget_list)
+void add_adjustments (GList *widget_list, GList **adj_list)
 {
-  g_list_foreach (widget_list, create_adjustment, 0);
+  g_list_foreach (widget_list, create_adjustment, adj_list);
+}
+
+void display_adjustment(gpointer data, gpointer user_data)
+{
+  struct adjustor *adjustor = data;
+  
+  if (GTK_IS_ADJUSTMENT(adjustor->adj)) {
+    printf("Id %s : value %d\n", adjustor->id, gtk_adjustment_get_value(adjustor->adj));
+  }
 }
 
 void
 create_adjustments_list (GtkWidget *top_widget)
 {
-  create_adjustment(top_widget, 0);
+  GList *adjustment_list = NULL;
+  create_adjustment(top_widget, &adjustment_list);
+
+  g_list_foreach (adjustment_list, display_adjustment, 0);
 }
 
 int
