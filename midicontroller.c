72bc376daba90d41f5506e42cd1fda4cfa17c6eb
Ricard Wanderlof <ricard2013@butoba.net>
2014-01-13 23:14:30 +0100

    Added function handlers for adjustments, sliders, comboboxes and buttons

--- midicontroller.c
+++ midicontroller.c
@@ -11,6 +11,48 @@ on_winMain_destroy (GtkObject *object, gpointer user_data)
   gtk_main_quit();
 }
 
+void
+on_adjustment_value_changed (GtkObject *object, gpointer user_data)
+{
+  GtkAdjustment *adj = GTK_ADJUSTMENT (object);
+  printf("Got value changed: adj %p\n", adj);
+  if (adj)
+    printf("Adj %p: name %s, value %d, user_data %p\n",
+           adj, gtk_widget_get_name(GTK_WIDGET(object)),
+           (int) gtk_adjustment_get_value(adj), user_data);
+}
+
+void
+on_value_changed (GtkObject *object, gpointer user_data)
+{
+  GtkRange *range = GTK_RANGE (object);
+  if (range)
+    printf("Slider %p: name %s, value %d, user_data %p\n",
+           range, gtk_buildable_get_name(GTK_BUILDABLE(range)),
+           (int) gtk_range_get_value(range), user_data);
+}
+
+
+void
+on_combobox_changed (GtkObject *object, gpointer user_data)
+{
+  GtkComboBox *cb = GTK_COMBO_BOX (object);
+  if (cb)
+    printf("Combobox %p: name %s, value %d, user_data %p\n",
+           cb, gtk_buildable_get_name(GTK_BUILDABLE(cb)),
+           gtk_combo_box_get_active(cb), user_data);
+}
+
+void
+on_button_pressed (GtkObject *object, gpointer user_data)
+{
+  GtkButton *button = GTK_BUTTON (object);
+  if (button)
+    printf("Button %p: name %s, user_data %p\n",
+           button, gtk_buildable_get_name(GTK_BUILDABLE(button)), user_data);
+}
+
+
 void add_adjustments(GList *widget_list, GList **adj_list);
 
 void create_adjustment (gpointer data, gpointer user_data)
@@ -50,7 +92,7 @@ void display_adjustment(gpointer data, gpointer user_data)
   struct adjustor *adjustor = data;
   
   if (GTK_IS_ADJUSTMENT(adjustor->adj)) {
-    printf("Id %s : value %d\n", adjustor->id, gtk_adjustment_get_value(adjustor->adj));
+    printf("Id %s (%p): value %d\n", adjustor->id, adjustor->adj, gtk_adjustment_get_value(adjustor->adj));
   }
 }
 
