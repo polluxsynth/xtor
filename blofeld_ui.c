56a0eee0dc9fcf31a1250e538c08c5772ef90059
Ricard Wanderlof <ricard2013@butoba.net>
2014-02-01 23:49:02 +0100

    Added selectable mod routing on Sound page
    * This is accomplished by basically copying the whole mod matrix from the
      Modulation page, and only showing the corresponding modulation path
      as governed by the Modulation Select combo box.

--- blofeld_ui.c
+++ blofeld_ui.c
@@ -39,6 +39,87 @@ on_Paste_Arp_pressed (GtkObject *object, gpointer user_data)
 }
 
 
+struct match {
+  const char *prefix;
+  int select;
+};
+
+/* Show/hide widget depending on number in parameter name */
+static void show_hide(gpointer data, gpointer user_data)
+{
+  GtkWidget *widget = data;
+  struct match *match = user_data;
+  const char *id = gtk_buildable_get_name(GTK_BUILDABLE(widget));
+  int number;
+
+  if (!id) return;
+
+  const char *find = strstr(id, match->prefix);
+  if (!find || find != id) return; /* Not at start of id */
+  /* try to convert what comes after to an integer. Exit otherwise. */
+  /* This means we exit when we find 'Modulation Select' for instance */
+  if (sscanf(id + strlen(match->prefix), "%d", &number) != 1) return;
+  /* Now we now we've got something numeric after the prefix, i.e. the
+   * number of the modulation route. show/hide depending on whether its
+   * the route we want to show or not. */
+  if (number == match->select)
+    gtk_widget_show(widget);
+  else
+    gtk_widget_hide(widget);
+}
+
+/* When Modulation Select changed, show relevant modulation routing */
+void
+on_Modulation_Select_changed (GtkObject *object, gpointer user_data)
+{
+  struct match match;
+  const char *id = gtk_buildable_get_name(GTK_BUILDABLE(object));
+  if (!id) return;
+  if (!GTK_IS_COMBO_BOX(object)) return;
+  int select = gtk_combo_box_get_active(GTK_COMBO_BOX(object));
+
+  /* Now mangle parameter name (= widget id) to something matchable */
+  /* The name of our widget is something like "Modulation Select", whereas
+   * the names of the widgets we want to hide/show are like
+   * "Modulation 1 Amount". So we grab 'Modulation' and use it to match
+   * against the child widgets to see which ones are interesting at all
+   * (several of them will be things like labels etc), and use
+   * the value of the combobox to match the actual Modulation route number. */
+
+  /* We generate a 'prefix' string which is the first word of the
+   * parameter, including the following space, e.g. "Modulation ". */
+
+  /* Need to make copy of string in order to modify it. */
+  char prefix[strlen(id) + 1];
+  strcpy(prefix, id);
+  /* Find end of first word, normally Modulation */
+  char *find = prefix;
+  while (*find && *find++ != ' ')
+    ;
+  if (!*find) return; /* end of string found before end of first word,
+                         or no suffix, i.e. id is just "Modulation " */
+  *find = '\0'; /* terminate it: we now have "Modulation " */
+
+  match.prefix = prefix;
+  match.select = select + 1; /* Assume routes start at 1, i.e. "Modulation 1" */
+
+  /* Now show/hide all children of parent container: Show the parameter
+   * with matching (first part of) name and number, and hide the others. */
+
+  /* Actually, we cheat a bit here, we just scan all children of the
+   * immediate parent, which is fine for the modulation where all the
+   * step widgets are in the same box; on a more global scale we'd have
+   * to start at the top window, and recursively scan every container we
+   * find. However, since we have two modulation routings, we couldn't go
+   * that high in the object hierarchy, or we would show/hide the routings
+   * for both modulation boxes. */
+  GtkWidget *container = gtk_widget_get_parent(GTK_WIDGET(object));
+  GList *container_children = gtk_container_get_children(GTK_CONTAINER(container));
+
+  g_list_foreach(container_children, show_hide, &match);
+}
+
+
 struct all_updater {
   const char *format;
   int value;
