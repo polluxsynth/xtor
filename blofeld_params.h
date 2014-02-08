450a69541d560b86ccf7190448d312968bac5975
Ricard Wanderlof <ricard2013@butoba.net>
2014-02-08 07:20:01 +0100

    Cleanup: Added GPL2 file headers to all source files

--- blofeld_params.h
+++ blofeld_params.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+ * midiedit - GTK based editor for MIDI synthesizers
+ *
+ * blofeld_params.h - Parameter management for Waldorf Blofeld.
+ * 
+ * Copyright (C) 2014  Ricard Wanderlof <ricard2013@butoba.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ****************************************************************************/
+
 #define BLOFELD_PARAMS 383
 
 #define PARNOS_ARPEGGIATOR 311, 358
