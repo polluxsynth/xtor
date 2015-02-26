74f1434a5ab518208daf0496d4ac6438e478477c
Ricard Wanderlof <ricard2013@butoba.net>
2015-02-26 22:32:14 +0100

    GTK_WIDGET_VISIBLE() is deprecated
    * Use gtk_widget_get_visible() since gtk 2.18

--- blofeld_knobs.c
+++ blofeld_knobs.c
@@ -79,7 +79,7 @@ print_knob(gpointer data, gpointer user_data)
   printf("Widget %s:%s (%d,%d): %s\n",
          gtk_widget_get_name(widget), gtk_buildable_get_name(GTK_BUILDABLE(widget)),
          widget->allocation.x, widget->allocation.y,
-         GTK_WIDGET_VISIBLE(widget) ? "visible" : "hidden");
+         gtk_widget_get_visible(widget) ? "visible" : "hidden");
 }
 
 static void
@@ -103,7 +103,7 @@ add_if_active(gpointer data, gpointer user_data)
   struct knob_descriptor *knob_descriptor = data;
   GList **active_list = user_data;
 
-  if (GTK_WIDGET_VISIBLE(knob_descriptor->widget))
+  if (gtk_widget_get_visible(knob_descriptor->widget))
     *active_list = g_list_prepend(*active_list, knob_descriptor);
 }
 
